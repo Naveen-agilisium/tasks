faa2223be027970e0b28b8986c08a96f273641ac
Naveen Sekar <Naveen.Sekar@agilisium.com>
2023-10-24 21:50:44 +0530

    taskTable with sorting and pagination

--- src/task/TaskTable.js
+++ src/task/TaskTable.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import {AiFillCloseCircle,AiFillEdit} from "react-icons/ai";
-import {flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
-import {format} from 'date-fns'
-
+import {AiFillCloseCircle,AiFillEdit,AiOutlineDelete,AiOutlineSearch,AiFillHeart} from "react-icons/ai";
+import {MdImportExport} from "react-icons/md"
+import { BiCaretLeft,BiCaretRight} from "react-icons/bi";
+import {flexRender, getCoreRowModel, useReactTable, getPaginationRowModel,getSortedRowModel} from "@tanstack/react-table";
 
 function TaskTable() {
     const [tasks, setTasks] = React.useState(
@@ -22,6 +22,7 @@ function TaskTable() {
         developedBy: "Naveen",
         updatedAt: new Date(),
         assignee: "Naveen",
+        type:"",
       };
       
       const[defValue,setDefValue]= useState(defaultValue);
@@ -29,7 +30,7 @@ function TaskTable() {
         {
             accessorKey : 'date',
             header: "DATE",
-            cell:(props)=><p>{format(new Date(props.getValue()), "dd-MMM-yyyy  hh:mm a") }</p>
+            cell:(props)=><p>{props.getValue()}</p>
         },
         {
             accessorKey : 'task',
@@ -54,7 +55,7 @@ function TaskTable() {
         {
             accessorKey : 'updatedAt',
             header: "Updated At",
-            cell:(props)=><p>{format(new Date(props.getValue()), "dd-MMM-yyyy  hh:mm a") }</p>
+            cell:(props)=><p>{props.getValue()}</p>
         },
         {
             accessorKey : 'assignee',
@@ -63,47 +64,94 @@ function TaskTable() {
         },
         {
             accessorKey:'id',
-            header: 'action',
-            cell: (props)=><button   onClick={editHandle} id={props.getValue()}><AiFillEdit/></button>
-
+            header: 'Action',
+            enableSorting: false, 
+            cell: (props)=>(
+              <div className='flex justify-evenly'>
+            <button   onClick={()=>editHandle (props.getValue())} id={props.getValue()}><AiFillEdit/></button>
+            <button   onClick={()=>deleteHandle (props.getValue())} id={props.getValue()}><AiOutlineDelete/></button>
+            </div>
+            )
+        },
+        {
+            accessorKey:'id',
+            enableSorting: false, 
+            cell: (props)=><span  className='text-[#dbd9d9] flex justify-center text-2xl' onClick={()=>favHandle (props.getValue())} id={props.getValue()}><AiFillHeart/></span>
         }
     ]
 
     const table =useReactTable({
         data:tasks,
         columns:columns,
-        getCoreRowModel:getCoreRowModel()
+        getCoreRowModel:getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        initialState: {
+            pagination: {
+              pageSize: 10,
+            },
+            sorting: [
+              {
+                id: "date",
+                desc: true,
+              },
+            ],
+          },
     });
    const addItem =(event)=>{
-    event.preventDefault();
-    console.log(tasks);
-    setTasks((tasks)=>[
-        ...tasks,
-        {
-        id:  Math.floor(1000 + Math.random() * 9000),
-        date:  new Date(),
-        task: defValue.task,
-        description: defValue.description,
-        status: defValue.status,
-        developedBy: defValue.developedBy,
-        updatedAt:  new Date(),
-        assignee: defValue.assignee, 
-        },
-    ]);
+    
+    event?.preventDefault();
+    if(defValue.id){
+
+      setTasks(tasks.map((value)=>
+        value.id === defValue.id ? {
+            ...defValue,
+            updatedAt:  new Date().toLocaleString(),
+        }: value))
+//console.log(va);
+        
+    }
+    else{
+        setTasks((tasks)=>[
+            ...tasks,
+            {
+            id:  Math.floor(1000 + Math.random() * 9000),
+            date:  new Date().toLocaleString(),
+            task: defValue.task,
+            description: defValue.description,
+            status: defValue.status,
+            developedBy: defValue.developedBy,
+            updatedAt:  new Date().toLocaleString(),
+            assignee: defValue.assignee, 
+            },
+        ]);
+    }
     setDefValue(defaultValue);
+    setAddModel(false);
+  
    }
 
-   const editHandle =(event)=>{
+   const editHandle =(idValue)=>{
     //console.log(event.target.id);
-    event.preventDefault();
+   // event.preventDefault();
     let valnew = tasks;
 
     const res = valnew.find((e)=>{
-        return e.id == event.currentTarget.id
+        return e.id == idValue
     })
     setDefValue(res);
     setAddModel(true);
 
+   }
+   const deleteHandle = (idValue)=>{
+
+    let valnew = tasks;
+    const res = valnew.filter(item=> item.id !== idValue);
+    console.log(res);
+    setTasks(res);
+   }
+   const favHandle = ()=>{
+    alert("Fav click pannita")
    }
   return (
   <>
@@ -147,6 +195,8 @@ function TaskTable() {
         </div>
         <div className='w-full text-center my-5'>
         <button onClick={addItem} className='w-[150px]  p-3 bg-red-500 text-white rounded-md font-[600]'>Add</button>
+
+        
         </div>
         
             </div>
@@ -158,24 +208,32 @@ function TaskTable() {
 }
    
   <button onClick= {()=>{setDefValue(defaultValue);setAddModel(!addmodel)}} className='w-[100px] rounded-sm bg-red-600 p-[10px] text-white hover:bg-red-500'>Add Task</button>
+  <div className='lg:w-[300px] float-right relative hidden'>
+  <input type='text' className=' w-full rounded-lg outline-none  p-[10px]  border border-gray-300'/>
+  <AiOutlineSearch className='absolute top-[12px] right-[10px] text-[18px]'/>
+  </div>
   <div className='tableArea  mt-6'>
   <table className='w-full border-collapse'>
     <thead>
     {table.getHeaderGroups().map((headerGroup)=><tr className='text-white bg-[#362F4B] ' key={headerGroup.id}>
         {headerGroup.headers.map(
-            header =><th  className='p-3 text-center first:rounded-tl-md last:rounded-tr-md'  key={header.id}>
-                {header.column.columnDef.header}
+            header =><th  className='p-3 text-center first:rounded-tl-md last:rounded-tr-md cursor-pointer'  key={header.id}>
+                 <div className='flex justify-center items-center' onClick={header.column.getToggleSortingHandler()}>
+                  {header.column.columnDef.header}
+                  {header.column.getCanSort() && (
+                    <span className='text-[20px]'><MdImportExport/></span>
+                  )}
+                  </div>
             </th>
         )}
     </tr>
   )}
   </thead>
   <tbody>
-    {console.log("table",table.getRowModel().rows)}
      {table.getRowModel().rows.map((row)=>(
         <tr className="odd:bg-white even:bg-[#eaeaea]" key={row.id}>
             {row.getVisibleCells().map((cell)=>(
-                <td className=' p-3 text-center ' key={cell.id}>
+                <td className='p-3 text-center ' key={cell.id}>
                     {flexRender(cell.column.columnDef.cell,cell.getContext())}
                 </td>
             ))}
@@ -184,6 +242,27 @@ function TaskTable() {
   </tbody>
   </table>
   </div>
+  <div className='my-5'>
+      <p className='flex items-center'>
+      <button className='p-2  bg-gray-200 text-[20px] rounded-tl-lg rounded-bl-lg'
+          onClick={() => table.previousPage()}
+          isDisabled={!table.getCanPreviousPage()}
+        >
+          {<BiCaretLeft/>}
+        </button>
+        <span className='p-2 bg-gray-200  border-x-2 border-white'>  Page {table.getState().pagination.pageIndex + 1} of{" "}
+        {table.getPageCount()}</span>
+      
+        <button className=' p-2  bg-gray-200 text-[20px] rounded-tr-lg rounded-br-lg'
+          onClick={() => {
+            if(table.getCanNextPage()) table.nextPage();
+        //   isDisabled={!table.getCanNextPage()}
+          }}
+        >
+           {<BiCaretRight/>}</button>
+      </p>
+    
+      </div>
   </div>
  
   
